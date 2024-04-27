6b6a97d52ecc368cbf930ee9e1cbe594eb0101dd
dharinibaskaran <143563246+dharinibaskaran@users.noreply.github.com>
2024-04-27 16:36:45 -0600

    Update config.py

--- config.py
+++ config.py
@@ -1,16 +1,15 @@
 class Config:
 
-    SQLALCHEMY_DATABASE_URI = 'db_uri'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://worldof1_bigdata:minions2024*@localhost:3306/worldof1_bigdata'
 
     SECRET_KEY = 'your-secret-key'
 
     JWT_SECRET_KEY = 'jwt-secret-string'
 
-    OPENAI_API_KEY = 'openai_api_key'
+    OPENAI_API_KEY = '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'
 
-    assistant_id = 'assistant_id'
+    assistant_id = 'asst_IX6SHYvaDrRjUxyWn45QnXlu'
 
-    entrez_access_email_id = 'youremail@colorado.edu'
+    entrez_access_email_id = 'dhba507@colorado.edu'
 
     # Add other configuration settings as needed
-
